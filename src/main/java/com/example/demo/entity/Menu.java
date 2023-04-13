11565579495d79d89af72860039fec1a3efa8704
Honghong <jihong700@naver.com>
2023-04-13 20:28:25 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/entity/Menu.java
@@ -0,0 +1,19 @@
+package com.example.demo.entity;
+
+public class Menu {
+
+    private String restaurant_Id; // 음식점
+
+    private Long menu_Id; // 음식 id
+
+    private String name; // 음식이름
+
+    private int price; // 음식가격
+
+    private String foodDetail; // 음식상세설명
+
+    private Integer quantity; // 수량
+
+    private int soldOut; // 음식품절
+
+}
