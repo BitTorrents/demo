81f0578e0ecf741782f7c3732eb287f8d15bf1f3
Honghong <jihong700@naver.com>
2023-04-27 23:13:23 +0900

    Initial commit

--- src/main/java/com/example/demo/controller/MenuController.java
+++ src/main/java/com/example/demo/controller/MenuController.java
@@ -8,25 +8,26 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class MenuController {
 
     private final MenuService menuService;
 
-    @GetMapping(value = "api/menu/new")
+    @GetMapping(value = "menu/new")
     public Long insert(@RequestBody Menu menu) {
         return menuService.save(menu);
     }
-    @GetMapping(value = "api/menu/{id}")
+    @GetMapping(value = "menu/{id}")
     public Menu read(@PathVariable Long id) {
         return menuService.findById(id);
     }
 
-    @GetMapping(value = "api/menu/{id}/update")
+    @GetMapping(value = "menu/{id}/update")
     public Long update(@PathVariable Long id,@RequestBody String name,@RequestBody int price,@RequestBody String foodDetail ) {
         return menuService.update(id, name, price, foodDetail);
     }
 
-    @GetMapping(value = "api/menu/{id}/delete")
+    @GetMapping(value = "menu/{id}/delete")
     public Long delete(@PathVariable Long id) {
         menuService.delete(id);
         return id;
